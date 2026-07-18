247a7f0ca33d62cb42e5e9f48d90f58798c36438
icedman <m4rvin2005@gmail.com>
2026-07-18 21:17:27 +0800

    Remove WrapMap.

--- src/root.zig
+++ src/root.zig
@@ -12,13 +12,6 @@ pub const Utf16SeekTarget = @import("Rope.zig").Utf16SeekTarget;
 pub const Point = @import("Rope.zig").Point;
 pub const TreeMap = @import("TreeMap.zig").TreeMap;
 pub const TreeSet = @import("TreeMap.zig").TreeSet;
-pub const WrapMap = @import("WrapMap.zig").WrapMap;
-pub const LineWrapEntry = @import("WrapMap.zig").LineWrapEntry;
-pub const BufferPoint = @import("WrapMap.zig").BufferPoint;
-pub const DisplayPoint = @import("WrapMap.zig").DisplayPoint;
-pub const expandTabs = @import("WrapMap.zig").expandTabs;
-pub const rawToExpanded = @import("WrapMap.zig").rawToExpanded;
-pub const expandedToRaw = @import("WrapMap.zig").expandedToRaw;
 pub const tests = @import("tests.zig");
 
 test {
